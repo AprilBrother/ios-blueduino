67cfecab2f87348c93c7b4e60ce423ccfddce0a5
daisysomus <daisysomus@gmail.com>
2014-08-26 16:00:18 +0800

    add firmata portocol

---
+++ ABStandFirmataSketch/ABFirmata.cpp
@@ -0,0 +1,444 @@
+//
+//  ABFirmata.cpp
+//  ABDuino
+//
+//  Created by liaojinhua on 14-8-19.
+//  Copyright (c) 2014年 AprilBrother. All rights reserved.
+//
+
+//******************************************************************************
+//* Includes
+//******************************************************************************
+
+#include "ABFirmata.h"
+#include "Boards.h"
+
+extern "C" {
+#include <string.h>
+#include <stdlib.h>
+}
+
+//******************************************************************************
+//* Support Functions
+//******************************************************************************
+
+void ABFirmataClass::sendValueAsTwo7bitBytes(int value)
+{
+    FirmataSerial->write(value & B01111111);
+    FirmataSerial->write(value >> 7 & B01111111);
+}
+
+void ABFirmataClass::startSysex()
+{
+    FirmataSerial->write(START_SYSEX);
+}
+
+void ABFirmataClass::endSysex()
+{
+    FirmataSerial->write(END_SYSEX);
+}
+
+//******************************************************************************
+//* Constructors
+//******************************************************************************
+
+ABFirmataClass::ABFirmataClass()
+{
+    firmwareVersionCount = 0;
+    firmwareVersionVector = 0;
+    systemReset();
+    FirmataSerial = new SoftwareSerial(11, 12);
+}
+
+//******************************************************************************
+//* Public Methods
+//******************************************************************************
+
+/* begin method with default serila bitrate */
+void ABFirmataClass::begin()
+{
+    begin(9600);
+}
+
+/* begin method for overriding default serial bitrate */
+void ABFirmataClass::begin(long speed)
+{
+    FirmataSerial->begin(speed);
+}
+
+// outpu the protocol version message to the serial port
+void ABFirmataClass::printVersion()
+{
+    FirmataSerial->write(REPORT_VERSION);
+    FirmataSerial->write(FIRMATA_MAJOR_VERSION);
+    FirmataSerial->write(FIRMATA_MINOR_VERSION);
+}
+
+void ABFirmataClass::blinkVersion()
+{
+    // flash the pin with the protocol version
+    pinMode(VERSION_BLINK_PIN, OUTPUT);
+    strobeBlinkPin(FIRMATA_MAJOR_VERSION, 40, 210);
+    delay(1000);
+    strobeBlinkPin(FIRMATA_MINOR_VERSION, 40, 210);
+    delay(1000);
+}
+
+void ABFirmataClass::printFirmwareVersion()
+{
+    byte i;
+    if (firmwareVersionCount) {
+        // make sure that the name has been set before reporting
+        startSysex();
+        FirmataSerial->write(REPORT_FIRMWARE);
+        FirmataSerial->write(firmwareVersionVector[0]); // major version number
+        FirmataSerial->write(firmwareVersionVector[1]); // minor version number
+        for (i = 2; i < firmwareVersionCount; ++i) {
+            sendValueAsTwo7bitBytes(firmwareVersionVector[i]);
+        }
+        endSysex();
+    }
+}
+
+void ABFirmataClass::setFirmwareNameAndVersion(const char *name, byte major, byte minor)
+{
+    const char *filename;
+    char *extension;
+    
+    // parse out ".cpp" that come from using __FILE__
+    extension = strstr(name, ".cpp");
+    filename = strrchr(name, '/') + 1; // points to slash, +1 gets to start of filename
+    // add two tytes for version numbers
+    if (extension && filename) {
+        firmwareVersionCount = extension - filename + 2;
+    } else {
+        firmwareVersionCount = strlen(name) + 2;
+        filename = name;
+    }
+    
+    free(firmwareVersionVector);
+    
+    firmwareVersionVector = (byte *) malloc (firmwareVersionCount);
+    firmwareVersionVector[firmwareVersionCount] = 0;
+    firmwareVersionVector[0] = major;
+    firmwareVersionVector[1] = minor;
+    strncpy((char *)firmwareVersionVector + 2, filename, firmwareVersionCount - 2);
+}
+
+int ABFirmataClass::available()
+{
+    return FirmataSerial->available();
+}
+
+void ABFirmataClass::processSysexMessage()
+{
+   switch (storedInputData[0]) {
+     case REPORT_FIRMWARE:
+         printFirmwareVersion();
+         break;
+      case STRING_DATA:
+         if (currentStringCallback) {
+             byte bufferLength = (sysexBytesRead - 1) / 2;
+             char *buffer = (char *)malloc(bufferLength * sizeof(char));
+             byte i = 1;
+             byte j = 0;
+             while (j < bufferLength) {
+                 buffer[j] = (char)storedInputData[i];
+                 i++;
+                 buffer[j] += (char)(storedInputData[i] << 7);
+                 i++;
+                 j++;
+             }
+             (*currentStringCallback)(buffer);
+         }
+         break;
+      default:
+           if (currentSysexCallback) {
+               (*currentSysexCallback)(storedInputData[0], sysexBytesRead - 1, storedInputData + 1);
+           }
+   }
+}
+
+void ABFirmataClass::processInput(void)
+{
+  int inputData = FirmataSerial->read(); // this is 'int' to handle -1 when no data
+  int command;
+    
+  // TODO make sure it handles -1 properly
+  
+  if (parsingSysex) {
+    if(inputData == END_SYSEX) {
+      //stop sysex byte      
+      parsingSysex = false;
+      //fire off handler function
+      processSysexMessage();
+    } else {
+      //normal data byte - add to buffer
+      storedInputData[sysexBytesRead] = inputData;
+      sysexBytesRead++;
+    }
+  } else if( (waitForData > 0) && (inputData < 128) ) {  
+    waitForData--;
+    storedInputData[waitForData] = inputData;
+    if( (waitForData==0) && executeMultiByteCommand ) { // got the whole message
+      switch(executeMultiByteCommand) {
+      case ANALOG_MESSAGE:
+        if(currentAnalogCallback) {
+          (*currentAnalogCallback)(multiByteChannel,
+                                   (storedInputData[0] << 7)
+                                   + storedInputData[1]);
+        }
+        break;
+      case DIGITAL_MESSAGE:
+        if(currentDigitalCallback) {
+          (*currentDigitalCallback)(multiByteChannel,
+                                    (storedInputData[0] << 7)
+                                    + storedInputData[1]);
+        }
+        break;
+      case SET_PIN_MODE:
+        if(currentPinModeCallback)
+          (*currentPinModeCallback)(storedInputData[1], storedInputData[0]);
+        break;
+      case REPORT_ANALOG:
+        if(currentReportAnalogCallback)
+          (*currentReportAnalogCallback)(multiByteChannel,storedInputData[0]);
+        break;
+      case REPORT_DIGITAL:
+        if(currentReportDigitalCallback)
+          (*currentReportDigitalCallback)(multiByteChannel,storedInputData[0]);
+        break;
+      }
+      executeMultiByteCommand = 0;
+    }	
+  } else {
+    // remove channel info from command byte if less than 0xF0
+    if(inputData < 0xF0) {
+      command = inputData & 0xF0;
+      multiByteChannel = inputData & 0x0F;
+    } else {
+      command = inputData;
+      // commands in the 0xF* range don't use channel data
+    }
+    switch (command) {
+    case ANALOG_MESSAGE:
+    case DIGITAL_MESSAGE:
+    case SET_PIN_MODE:
+      waitForData = 2; // two data bytes needed
+      executeMultiByteCommand = command;
+      break;
+    case REPORT_ANALOG:
+    case REPORT_DIGITAL:
+      waitForData = 1; // two data bytes needed
+      executeMultiByteCommand = command;
+      break;
+    case START_SYSEX:
+      parsingSysex = true;
+      sysexBytesRead = 0;
+      break;
+    case SYSTEM_RESET:
+      systemReset();
+      break;
+    case REPORT_VERSION:
+      ABFirmata.printVersion();
+      break;
+    }
+  }
+}
+
+//------------------------------------------------------------------------------
+// Serial Send Handling
+
+// send an analog message
+void ABFirmataClass::sendAnalog(byte pin, int value) 
+{
+  // pin can only be 0-15, so chop higher bits
+  FirmataSerial->write(ANALOG_MESSAGE | (pin & 0xF));
+  sendValueAsTwo7bitBytes(value);
+}
+
+// send a single digital pin in a digital message
+void ABFirmataClass::sendDigital(byte pin, int value) 
+{
+  /* TODO add single pin digital messages to the protocol, this needs to
+   * track the last digital data sent so that it can be sure to change just
+   * one bit in the packet.  This is complicated by the fact that the
+   * numbering of the pins will probably differ on Arduino, Wiring, and
+   * other boards.  The DIGITAL_MESSAGE sends 14 bits at a time, but it is
+   * probably easier to send 8 bit ports for any board with more than 14
+   * digital pins.
+   */
+
+  // TODO: the digital message should not be sent on the serial port every
+  // time sendDigital() is called.  Instead, it should add it to an int
+  // which will be sent on a schedule.  If a pin changes more than once
+  // before the digital message is sent on the serial port, it should send a
+  // digital message for each change.
+
+  //    if(value == 0)
+  //        sendDigitalPortPair();
+}
+
+
+// send 14-bits in a single digital message (protocol v1)
+// send an 8-bit port in a single digital message (protocol v2)
+void ABFirmataClass::sendDigitalPort(byte portNumber, int portData)
+{
+  FirmataSerial->write(DIGITAL_MESSAGE | (portNumber & 0xF));
+  FirmataSerial->write((byte)portData % 128); // Tx bits 0-6
+  FirmataSerial->write(portData >> 7);  // Tx bits 7-13
+}
+
+
+void ABFirmataClass::sendSysex(byte command, byte bytec, byte* bytev) 
+{
+  byte i;
+  startSysex();
+  FirmataSerial->write(command);
+  for(i=0; i<bytec; i++) {
+    sendValueAsTwo7bitBytes(bytev[i]);        
+  }
+  endSysex();
+}
+
+void ABFirmataClass::sendString(byte command, const char* string) 
+{
+  sendSysex(command, strlen(string), (byte *)string);
+}
+
+
+// send a string as the protocol string type
+void ABFirmataClass::sendString(const char* string) 
+{
+  sendString(STRING_DATA, string);
+}
+
+// expose the write method
+void ABFirmataClass::write(byte c)
+{
+  FirmataSerial->write(c);
+}
+
+
+// Internal Actions/////////////////////////////////////////////////////////////
+
+// generic callbacks
+void ABFirmataClass::attach(byte command, callbackFunction newFunction)
+{
+  switch(command) {
+  case ANALOG_MESSAGE: currentAnalogCallback = newFunction; break;
+  case DIGITAL_MESSAGE: currentDigitalCallback = newFunction; break;
+  case REPORT_ANALOG: currentReportAnalogCallback = newFunction; break;
+  case REPORT_DIGITAL: currentReportDigitalCallback = newFunction; break;
+  case SET_PIN_MODE: currentPinModeCallback = newFunction; break;
+  }
+}
+
+void ABFirmataClass::attach(byte command, systemResetCallbackFunction newFunction)
+{
+  switch(command) {
+  case SYSTEM_RESET: currentSystemResetCallback = newFunction; break;
+  }
+}
+
+void ABFirmataClass::attach(byte command, stringCallbackFunction newFunction)
+{
+  switch(command) {
+  case STRING_DATA: currentStringCallback = newFunction; break;
+  }
+}
+
+void ABFirmataClass::attach(byte command, sysexCallbackFunction newFunction)
+{
+  currentSysexCallback = newFunction;
+}
+
+void ABFirmataClass::detach(byte command)
+{
+  switch(command) {
+  case SYSTEM_RESET: currentSystemResetCallback = NULL; break;
+  case STRING_DATA: currentStringCallback = NULL; break;
+  case START_SYSEX: currentSysexCallback = NULL; break;
+  default:
+    attach(command, (callbackFunction)NULL);
+  }
+}
+
+// sysex callbacks
+/*
+ * this is too complicated for analogReceive, but maybe for Sysex?
+ void ABFirmataClass::attachSysex(sysexFunction newFunction)
+ {
+ byte i;
+ byte tmpCount = analogReceiveFunctionCount;
+ analogReceiveFunction* tmpArray = analogReceiveFunctionArray;
+ analogReceiveFunctionCount++;
+ analogReceiveFunctionArray = (analogReceiveFunction*) calloc(analogReceiveFunctionCount, sizeof(analogReceiveFunction));
+ for(i = 0; i < tmpCount; i++) {
+ analogReceiveFunctionArray[i] = tmpArray[i];
+ }
+ analogReceiveFunctionArray[tmpCount] = newFunction;
+ free(tmpArray);
+ }
+*/
+
+//******************************************************************************
+//* Private Methods
+//******************************************************************************
+
+
+
+// resets the system state upon a SYSTEM_RESET message from the host software
+void ABFirmataClass::systemReset(void)
+{
+  byte i;
+
+  waitForData = 0; // this flag says the next serial input will be data
+  executeMultiByteCommand = 0; // execute this after getting multi-byte data
+  multiByteChannel = 0; // channel data for multiByteCommands
+
+  for(i=0; i<MAX_DATA_BYTES; i++) {
+    storedInputData[i] = 0;
+  }
+
+  parsingSysex = false;
+  sysexBytesRead = 0;
+
+  if(currentSystemResetCallback)
+    (*currentSystemResetCallback)();
+}
+
+
+
+// =============================================================================
+// used for flashing the pin for the version number
+void ABFirmataClass::strobeBlinkPin(int count, int onInterval, int offInterval) 
+{
+  byte i;
+  pinMode(VERSION_BLINK_PIN, OUTPUT);
+  for(i=0; i<count; i++) {
+    delay(offInterval);
+    digitalWrite(VERSION_BLINK_PIN, LOW);
+    delay(onInterval);
+    digitalWrite(VERSION_BLINK_PIN, HIGH);
+  }
+}
+
+
+// make one instance for the user to use
+ABFirmataClass ABFirmata;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
